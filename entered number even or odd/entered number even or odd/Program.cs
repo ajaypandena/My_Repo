55c98b8a0e58f575f37dbbec166017a823239782
JDSPORTS\Ajay.Pandena <ajay.pandena@jdplc.com>
2024-07-12 06:34:14 +0100

    adding

---
+++ entered number even or odd/entered number even or odd/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace entered_number_even_or_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Enter a Number:");
+            i = int.Parse(Console.ReadLine());
+
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"Entered number {i} is Even ");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine($"Entered number {i} is Odd");
+                Console.Read();
+            }
+
+
+
+        }
+    }
+}
