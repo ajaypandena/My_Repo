d01c77d3bd5a491dd0c053b37d671a718e2996e3
JDSPORTS\Ajay.Pandena <ajay.pandena@jdplc.com>
2024-07-19 23:08:47 +0100

    prime numbers

---
+++ prime numbers/prime numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+            int x;
+            Console.WriteLine("Enter Number to check Prime Number : ");
+            x = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= 100; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    Console.Write("\t" + i);
+                }
+                isPrime = true;
+            }
+            Console.ReadKey();
+        }
+    }
+}
